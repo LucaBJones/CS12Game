65d12b1b8cf12912057645c7e0f07aacb00224a8
RosannaJ <77137623+RosannaJ@users.noreply.github.com>
2021-10-20 13:25:36 -0700

    Update Game.java
    - creates window when run

--- GroupGame/src/Game.java
+++ GroupGame/src/Game.java
@@ -1,9 +1,33 @@
+import java.awt.Canvas;
+import java.awt.Dimension;
 
-public class Game {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
+public class Game extends Canvas {
+	
+	private final int WINDOW_WIDTH = 500;
+	private final int WINDOW_HEIGHT = 500;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+		Game game = new Game();
+	} // Game
+	
+	public Game() {
+		JFrame frame = new JFrame("Game");
+		JPanel panel = new JPanel();
+		
+		setBounds(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+		
+		panel.setPreferredSize(new Dimension(WINDOW_WIDTH, WINDOW_HEIGHT));
+		panel.setLayout(null);
+		panel.add(this);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
+		frame.pack();
+		frame.setResizable(false);
+		frame.setVisible(true);
+	} // Game
+	
+} // Game
