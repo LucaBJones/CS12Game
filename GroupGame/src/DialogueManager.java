b1a52bfce68a7e6276de0082005b4d862751a02b
RosannaJ <77137623+RosannaJ@users.noreply.github.com>
2021-10-31 11:40:48 -0700

    Made dialogue stop displaying once finished
    - finished commenting DialogueManager and DialogueNode

--- GroupGame/src/DialogueManager.java
+++ GroupGame/src/DialogueManager.java
@@ -30,7 +30,7 @@ public class DialogueManager {
 	private int yPadding;
 	
 	private int x;
-	private int y; // inherit from Entity instead? (then can use Sprite too)
+	private int y;
 	
 	private Timer timer;
 	private int textSpeed;
@@ -51,7 +51,7 @@ public class DialogueManager {
 		timer = new Timer(textSpeed, new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 updateText();
-            }
+            } // actionPerformed
         });
 		
 		// can change
@@ -68,10 +68,12 @@ public class DialogueManager {
 		choiceHeight = (int) (Camera.getHeight() * 0.08);
 	} // DialogueManager
 	
+	// store dialogue node into dialogueStore
 	public void add(String id, DialogueNode node) {
 		dialogueStore.put(id, node);
-	}
+	} // add
 	
+	// start a dialogue with the id of the first node of the dialogue
 	public void start(String id) {
 		isDisplaying = true;
 		waitingForChoice = false;
@@ -79,24 +81,26 @@ public class DialogueManager {
 		currentDialogueID = id;
 		
 		timer.start();
-	}
+	} // start
 	
+	// animates the text (one letter at a time)
 	public void updateText() {
 		if (!isDisplaying) { return; } // needed?
 		if (index > dialogueStore.get(currentDialogueID).getText().length() - 1) { 
 			timer.stop();
 			textIsAnimating = false;
 			return;
-		}
+		} // if
 		
 		displayText += dialogueStore.get(currentDialogueID).getText().charAt(index);
 		index++;
-	}
+	} // updateText
 	
+	// updates to next dialogue node if there is one
 	public void update() {
 		if (!isDisplaying || waitingForChoice) { return; }
 		
-		// if text is animation, display full line of text
+		// if text is animating, display full line of text
 		if (textIsAnimating) {
 			displayText = dialogueStore.get(currentDialogueID).getText();
 			timer.stop();
@@ -108,7 +112,11 @@ public class DialogueManager {
 		String[] next = dialogueStore.get(currentDialogueID).getNext();
 		
 		// check if there is any more dialogue
-		if (next == null) { return; }
+		if (next == null) { 
+			isDisplaying = false;
+			System.out.println("no more dialogue, isDisplaying: " + isDisplaying);
+			return; 
+		} // if
 		
 		// if there are no choices, set dialogue
 		if (next.length == 1) {
@@ -136,6 +144,7 @@ public class DialogueManager {
 		textIsAnimating = true;
 	} // update
 	
+	// sets currentChoice to what the player has chosen
 	public void choose(int n) {
 		if (!waitingForChoice) { return; }
 		currentChoice = n;
@@ -143,6 +152,7 @@ public class DialogueManager {
 		update();
 	} // choose
 	
+	// handles clicking on a choice
 	public void handleClick(MouseEvent e) {
 		if (!isDisplaying) { return; }
 		
@@ -159,21 +169,24 @@ public class DialogueManager {
 		} // for
 	} // handleClick
 	
+	// draws the dialogue to the screen
 	public void draw(Graphics g) {
-		if (!dialogueStore.containsKey(currentDialogueID)) { return; }
+		if (!dialogueStore.containsKey(currentDialogueID) || !isDisplaying) { return; }
 		
 		// draw background (use sprites?)
 		g.setColor(Color.LIGHT_GRAY);
 		g.fillRect((int) x, (int) y, dialogueWidth, dialogueHeight);
 
+		// draw dialogue text
 		g.setFont(g.getFont().deriveFont(g.getFont().getSize() * 2f)); // temp
 		g.setColor(Color.black);
 		g.drawString(displayText, (int) x + 30, (int) y + 40); // can change
 	
-		// display speaker
+		// display speaker background
 		g.setColor(Color.LIGHT_GRAY);
 		g.fillRect((int) x , (int) y - 50, 160, 40);
 		
+		// display speaker
 		g.setFont(g.getFont().deriveFont(g.getFont().getSize() * 0.6f)); // temp
 		g.setColor(Color.BLACK);
 		g.drawString(dialogueStore.get(currentDialogueID).getSpeaker(), (int) x + 10, (int) y - 30);
@@ -184,15 +197,18 @@ public class DialogueManager {
 		} // if
 	} // draw
 	
+	// draws the choices to the screen
 	public void drawChoices(Graphics g) {
 		
 		for (int i = 0; i < currentChoiceNodes.length; i++) {
 			int choiceX = Camera.getWidth() - choiceWidth - xPadding;
 			int choiceY = (int) y - (choiceHeight) * (i + 1) - ((i + 1) * yPadding);
 			
+			// draw background
 			g.setColor(Color.LIGHT_GRAY);
 			g.fillRect(choiceX, choiceY, choiceWidth, choiceHeight);
 			
+			// draw text
 			g.setColor(Color.black);
 			g.drawString(dialogueStore.get(currentChoiceNodes[i]).getChoiceText(), choiceX + 10, choiceY + 20); // idk why we need to add more to the y... but it works...
 		} // for
--- GroupGame/src/DialogueNode.java
+++ GroupGame/src/DialogueNode.java
@@ -17,7 +17,7 @@ public class DialogueNode {
 		this.choiceText = "";
 		
 		d.add(id, this);
-	}
+	} // DialogueNode
 	
 	// choice dialogueNode (should there only be one constructor?)
 	public DialogueNode(String id, String speaker, String text, String choiceText, String[] nextIDs, DialogueManager d) {
@@ -30,21 +30,25 @@ public class DialogueNode {
 		this.choiceText = choiceText;
 		
 		d.add(id, this);
-	}
+	} // DialogueNode
 	
+	// returns the dialogue text
 	public String getText() {
 		return text;
 	} // getText
 	
+	// returns the name of the speaker
 	public String getSpeaker() {
 		return speaker;
-	}
+	} // getSpeaker
 	
+	// returns the id of the next dialogue node
 	public String[] getNext() {
 		return nextIDs;
 	} // getNext
 	
+	// returns the choice text
 	public String getChoiceText() {
 		return choiceText;
 	} // getChoiceText
-}
+} // DialogueNode
